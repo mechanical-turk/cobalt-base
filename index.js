40fd6f0564ebd5e17d29beb6920ac0a42023947d
Kerem Kazan <kerem.kazan@gmail.com>
2016-10-08 22:34:56 -0400

    argv-manager only parses argv now

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-const path = require('path');
 const { config } = require('cobalt-common');
-const argvManager = require('./lib/argv-manager.js');
+const { getOptions, getCommand } = require('./lib/argv-manager.js');
+const { loadGenerators } = require('cobalt-generator');
+
+function run() {
+  config.COMMANDS = {
+    generate: loadGenerators(),
+  };
+  const options = getOptions();
+  const command = getCommand(options);
+  command(options);
+}
 
 module.exports = {
   config,
-  run: argvManager.run,
+  run,
 }
