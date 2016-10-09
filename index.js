184df8ba07116feb24e53b40bcbc6c61886c66b6
Kerem Kazan <kerem.kazan@gmail.com>
2016-10-08 20:24:51 -0400

    moved argv form generator to here

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const path = require('path');
 const { config } = require('cobalt-common');
-const { run } = require('cobalt-generator');
-
 const argvManager = require('./lib/argv-manager.js');
 
 module.exports = {
   config,
-  run,
+  run: argvManager.run,
 }
