8b4e9749f822a6d1f5ed6e774271867c0d8f1da1
Kerem Kazan <kerem.kazan@gmail.com>
2016-10-08 20:11:29 -0400

    cobalt users will only need to import cobalt-base now

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const path = require('path');
+const { config } = require('cobalt-common');
 const { run } = require('cobalt-generator');
 
+const argvManager = require('./lib/argv-manager.js');
+
 module.exports = {
+  config,
   run,
 }
